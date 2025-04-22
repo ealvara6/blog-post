a342859c2e24b18c8a20c7fd9ffa96c8b77ba84c
Eduardo Alvarado <ealvara73@gmail.com>
2025-04-21 19:54:53 -0500

    Add mock comments file

---
+++ frontend/src/__mocks__/mockComments.ts
@@ -0,0 +1,32 @@
+export const mockComment = [
+  {
+    id: 101,
+    content: 'mock_content',
+    createdAt: new Date(),
+    userId: 1,
+    user: {
+      id: 1,
+      username: 'mock_username',
+    },
+  },
+  {
+    id: 101,
+    content: 'mock_content',
+    createdAt: new Date(),
+    userId: 1,
+    user: {
+      id: 1,
+      username: 'mock_username',
+    },
+  },
+  {
+    id: 101,
+    content: 'mock_content',
+    createdAt: new Date(),
+    userId: 1,
+    user: {
+      id: 1,
+      username: 'mock_username',
+    },
+  },
+]
