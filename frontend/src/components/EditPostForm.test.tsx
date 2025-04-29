95a3a2eb89ca96d0dd80e115d705f80cc77a3bca
Eduardo Alvarado <ealvara73@gmail.com>
2025-04-28 20:47:57 -0500

    Add edit form tests

---
+++ frontend/src/components/EditPostForm.test.tsx
@@ -0,0 +1,39 @@
+import { customRender } from '@/utils/test-utils'
+import { vi } from 'vitest'
+import { EditPostForm } from './EditPostForm'
+import { mockPosts } from '@/__mocks__/mockPosts'
+import { screen } from '@testing-library/react'
+
+const mockPost = mockPosts[0]
+
+describe('edit post', () => {
+  let titleInput: HTMLElement
+  let contentInput: HTMLElement
+  let saveButton: HTMLButtonElement
+
+  beforeEach(() => {
+    vi.clearAllMocks()
+    customRender(
+      <EditPostForm
+        title={mockPost.title}
+        content={mockPost.content}
+        id={mockPost.id}
+        userId={mockPost.userId}
+      />,
+    )
+    titleInput = screen.getByLabelText(/title:/i)
+    contentInput = screen.getByPlaceholderText(/content/i)
+    saveButton = screen.getByText(/submit/i)
+  })
+
+  it('renders the edit post form component', () => {
+    expect(titleInput).toBeInTheDocument()
+    expect(contentInput).toBeInTheDocument()
+    expect(saveButton).toBeInTheDocument()
+  })
+
+  it('displays the original values of the post in the input fields', () => {
+    expect(titleInput).toHaveValue('mock_title')
+    expect(contentInput).toHaveValue('mock_content')
+  })
+})
