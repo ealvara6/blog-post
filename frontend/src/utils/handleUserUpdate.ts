a327c8bdc7515dc4597d448dad799aa9f483ef0e
Eduardo Alvarado <ealvara73@gmail.com>
2025-05-14 21:09:07 -0500

    Add helper to handle user updates

---
+++ frontend/src/utils/handleUserUpdate.ts
@@ -0,0 +1,21 @@
+import { User } from '@/context/AuthProvider/AuthContext'
+import { EditUser } from '@/hooks/useUpdateUser'
+import { parseErrorMessage } from './parseErrorMessage'
+import React from 'react'
+
+export const handleUserUpdate = async (
+  updatedUser: User,
+  setAuthUser: React.Dispatch<React.SetStateAction<User>>,
+  updateUser: (id: number | undefined, data: EditUser) => Promise<User>,
+) => {
+  try {
+    console.log(updatedUser)
+    if (!updatedUser) return
+
+    await updateUser(updatedUser.id, updatedUser)
+    localStorage.setItem('user', JSON.stringify(updatedUser))
+    setAuthUser(updatedUser)
+  } catch (err) {
+    parseErrorMessage(err)
+  }
+}
