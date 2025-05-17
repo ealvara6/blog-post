4a3f5d3d4e098c110043dff9960aa4567a457da1
Eduardo Alvarado <ealvara73@gmail.com>
2025-05-16 22:12:25 -0500

    Update auth validation schemas

--- frontend/src/validations/authValidations.ts
+++ frontend/src/validations/authValidations.ts
@@ -1,29 +1,24 @@
 import { z } from 'zod'
 
-export const loginSchema = z.object({
-  email: z.string().trim().min(1, 'Email is required').email(),
-  password: z.string().min(1, 'Password is required'),
+const passwordFields = z.object({
+  password: z
+    .string()
+    .min(1, 'Password is required')
+    .min(7, 'Password must be at least 7 characters long'),
+  confirmPassword: z.string(),
 })
 
-export const signupSchema = z
-  .object({
-    username: z
-      .string()
-      .trim()
-      .min(1, 'Username is required')
-      .min(4, 'Username must be at least 4 characters long'),
-    email: z.string().trim().min(1, 'Email is required').email(),
-    password: z
-      .string()
-      .min(1, 'Password is required')
-      .min(7, 'Password must be at least 7 characters long'),
-    confirmPassword: z.string(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
+export const passwordSchema = passwordFields.refine(
+  (data) => data.password === data.confirmPassword,
+  {
     message: 'Passwords do not match',
     path: ['confirmPassword'],
-  })
+  },
+)
 
+export const emailSchema = z.object({
+  email: z.string().trim().min(1, 'Email is required').email(),
+})
 export const usernameSchema = z.object({
   username: z
     .string()
@@ -31,3 +26,15 @@ export const usernameSchema = z.object({
     .min(1, 'Username is required')
     .min(4, 'Username must be at least 4 characters long'),
 })
+
+export const loginSchema = emailSchema.extend({
+  password: z.string().min(1, 'Password is required'),
+})
+
+export const signupSchema = usernameSchema
+  .merge(emailSchema)
+  .extend(passwordFields.shape)
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  })
