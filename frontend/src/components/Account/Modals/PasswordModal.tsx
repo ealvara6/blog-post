7d0c7ec124fd728ffa59cad5ef51a8ac8f67094c
Eduardo Alvarado <ealvara73@gmail.com>
2025-05-18 21:24:10 -0500

    Update password modal file name

--- frontend/src/components/Account/Modals/PasswordModal.tsx
+++ frontend/src/components/Account/Modals/PasswordModal.tsx
@@ -36,14 +36,24 @@ export const PasswordModal = () => {
     <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
       <div className="flex flex-col gap-1">
         <label htmlFor="password">New Password: </label>
-        <Input {...register('password')} type="password" />
+        <Input
+          {...register('password')}
+          type="password"
+          name="password"
+          id="password"
+        />
         {errors['password'] && (
           <div className="text-red-500">{errors['password']?.message}</div>
         )}
       </div>
       <div className="flex flex-col gap-1">
         <label htmlFor="confirmPassword">Confirm Password: </label>
-        <Input {...register('confirmPassword')} type="password" />
+        <Input
+          {...register('confirmPassword')}
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+        />
         {errors['confirmPassword'] && (
           <div className="text-red-500">
             {errors['confirmPassword']?.message}
