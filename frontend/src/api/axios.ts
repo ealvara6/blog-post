1eba6cd4b7d4b11fdbde01e879ffa45c27ad18a4
Eduardo Alvarado <ealvara73@gmail.com>
2025-06-23 22:08:36 -0500

    update backend api to connect to railway project

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const LOCAL_STORAGE_KEY = 'token'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://blog-post-production-cb9e.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
