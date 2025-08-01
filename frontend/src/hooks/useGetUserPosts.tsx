ec11c9ce6aa9605313e5855a52d1e5e1a428a740
Eduardo Alvarado <ealvara73@gmail.com>
2025-07-31 22:31:40 -0500

    Remove no longer used hooks

--- frontend/src/hooks/useGetUserPosts.tsx
+++
@@ -1,9 +0,0 @@
-import api from '@/api/axios'
-import { useCallback } from 'react'
-
-export const useGetUserPosts = () => {
-  return useCallback(async (id: number) => {
-    const response = await api.get(`auth/users/${id}/posts`)
-    return response.data.data
-  }, [])
-}
