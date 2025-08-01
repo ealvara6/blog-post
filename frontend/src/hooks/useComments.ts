f3baeb35d57e4db2ffb29f07e085645c4126d598
Eduardo Alvarado <ealvara73@gmail.com>
2025-07-31 21:30:54 -0500

    Update useComments hook to use optimistic updates

--- frontend/src/hooks/useComments.ts
+++ frontend/src/hooks/useComments.ts
@@ -1,10 +1,21 @@
-import { createComment, getCommentsByPostId } from '@/api/commentsApi'
+import {
+  createComment,
+  deleteComment,
+  getCommentsByPostId,
+} from '@/api/commentsApi'
+import { Comment } from '@/types/posts'
 import { parseErrorMessage } from '@/utils/parseErrorMessage'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 type CreateDataFn = {
   content: string
   postId: number
+  username?: string
+}
+
+type DeleteDataFn = {
+  commentId: number
+  postId?: number
 }
 
 export const useComments = (postId: number) => {
@@ -21,13 +32,92 @@ export const useCreateComment = () => {
 
   return useMutation({
     mutationFn: (data: CreateDataFn) => createComment(data),
-    onSuccess: (data, variables) => {
+
+    onMutate: async (newComment) => {
+      await queryClient.cancelQueries({
+        queryKey: ['comments', newComment.postId],
+      })
+
+      const previousComments = queryClient.getQueryData([
+        'comments',
+        newComment.postId,
+      ])
+      queryClient.setQueryData<{ comments: Comment[] }>(
+        ['comments', newComment.postId],
+        (old) => ({
+          comments: [
+            ...(old?.comments ?? []),
+            {
+              id: Math.random(),
+              content: newComment.content,
+              postId: newComment.postId,
+              userId: Math.random(),
+              user: {
+                id: Math.random(),
+                username: newComment.username ?? 'You',
+              },
+              createdAt: new Date().toISOString(),
+            },
+          ],
+        }),
+      )
+
+      return { previousComments }
+    },
+
+    onError: (_err, newComment, context) => {
+      queryClient.setQueryData(
+        ['comments', newComment.postId],
+        context?.previousComments,
+      )
+    },
+
+    onSettled: (_data, _err, variables) => {
       queryClient.invalidateQueries({
-        queryKey: ['post', variables.postId],
+        queryKey: ['comments', variables.postId],
       })
     },
-    onError: (err) => {
-      throw parseErrorMessage(err)
+  })
+}
+
+export const useDeleteComment = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: (data: DeleteDataFn) => deleteComment(data.commentId),
+
+    onMutate: async (deletedComment) => {
+      await queryClient.cancelQueries({
+        queryKey: ['comments', deletedComment.postId],
+      })
+
+      const previousComments = queryClient.getQueryData([
+        'comments',
+        deletedComment.postId,
+      ])
+
+      queryClient.setQueryData<{ comments: Comment[] }>(
+        ['comments', deletedComment.postId],
+        (old) => ({
+          comments: (old?.comments ?? []).filter(
+            (comment: Comment) => comment.id !== deletedComment.commentId,
+          ),
+        }),
+      )
+
+      return { previousComments }
+    },
+    onError: (_err, newComment, context) => {
+      queryClient.setQueryData(
+        ['comments', newComment.postId],
+        context?.previousComments,
+      )
+    },
+
+    onSettled: (_data, _err, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ['comments', variables.postId],
+      })
     },
   })
 }
