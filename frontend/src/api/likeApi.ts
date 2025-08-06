0a26956a58fe02d03a9d3fb268a8789fdf329862
Eduardo Alvarado <ealvara73@gmail.com>
2025-08-05 19:08:48 -0500

    Create React Query functions to post likes and update Heart component to utilize them

--- frontend/src/api/likeApi.ts
+++ frontend/src/api/likeApi.ts
@@ -1,36 +1,72 @@
 import { parseErrorMessage } from '@/utils/parseErrorMessage'
 import api from './axios'
 
-export const likeComment = async (id: number) => {
+export const getPostLikes = async (postId: number) => {
   try {
-    const response = await api.post(`/auth/comments/${id}/like`)
+    const response = await api.get(`/posts/${postId}/likes`)
     return response.data
   } catch (err) {
     throw parseErrorMessage(err)
   }
 }
 
-export const unlikeComment = async (id: number) => {
+export const getUserLikedPost = async (postId: number) => {
   try {
-    const response = await api.delete(`/auth/comments/${id}/like`)
+    const response = await api.get(`/auth/posts/${postId}/like/me`)
     return response.data
   } catch (err) {
     throw parseErrorMessage(err)
   }
 }
 
-export const getCommentLikes = async (id: number) => {
+export const unlikePost = async (postId: number) => {
   try {
-    const response = await api.get(`/comments/${id}/likes`)
+    const response = await api.delete(`/auth/posts/${postId}/like`)
     return response.data
   } catch (err) {
     throw parseErrorMessage(err)
   }
 }
 
-export const getUserLikedComment = async (id: number) => {
+export const likePost = async (postId: number) => {
   try {
-    const response = await api.get(`/auth/comments/${id}/like/me`)
+    const response = await api.post(`/auth/posts/${postId}/like`)
+    return response.data
+  } catch (err) {
+    throw parseErrorMessage(err)
+  }
+}
+
+export const likeComment = async (commentId: number) => {
+  try {
+    const response = await api.post(`/auth/comments/${commentId}/like`)
+    return response.data
+  } catch (err) {
+    throw parseErrorMessage(err)
+  }
+}
+
+export const unlikeComment = async (commentId: number) => {
+  try {
+    const response = await api.delete(`/auth/comments/${commentId}/like`)
+    return response.data
+  } catch (err) {
+    throw parseErrorMessage(err)
+  }
+}
+
+export const getCommentLikes = async (commentId: number) => {
+  try {
+    const response = await api.get(`/comments/${commentId}/likes`)
+    return response.data
+  } catch (err) {
+    throw parseErrorMessage(err)
+  }
+}
+
+export const getUserLikedComment = async (commentId: number) => {
+  try {
+    const response = await api.get(`/auth/comments/${commentId}/like/me`)
     return response.data
   } catch (err) {
     throw parseErrorMessage(err)
