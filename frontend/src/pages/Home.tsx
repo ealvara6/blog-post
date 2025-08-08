876350119b2043b824449efce10a4875f8bd45cb
Eduardo Alvarado <ealvara73@gmail.com>
2025-08-07 22:16:25 -0500

    added margins on shorter screen width for home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   const { authUser } = useAuth()
   return (
     <div
-      className={`-mt-4 flex flex-col gap-15 ${authUser ? '' : 'sm:-mt-20'}`}
+      className={`-mt-4 flex flex-col gap-15 ${authUser ? 'mt-10 mb-10' : 'sm:-mt-20'}`}
     >
       {!authUser && <Hero />}
       <div className="max-w-screen">
